60100e99cd9162a23a1a8ee9029b5b3ee31d4aa5
AlnaDesarroller <natansanchez732@gmail.com>
2025-09-27 15:15:21 -0400

    Interfaces para los servicios

---
+++ HorizonFutureVest.BusinessLogic/Interfaces/IRankingService.cs
@@ -0,0 +1,11 @@
+﻿using HorizonFutureVest.BusinessLogic.DTOs;
+
+namespace HorizonFutureVest.BusinessLogic.Interfaces;
+
+public interface IRankingService
+{
+    Task<(bool Success, string ErrorMessage, List<RankingResultDto> Resultado)> GenerarRankingAsync(int year);
+    Task<(bool Success, string ErrorMessage, List<RankingResultDto> Resultado)> GenerarRankingSimulacionAsync(int year);
+    Task<IEnumerable<int>> GetYearConIndicadoresAsync();
+    Task<int> GetYearMasRecienteAsync();
+}
---
+++ HorizonFutureVest.BusinessLogic/Interfaces/ISimulacionService.cs
@@ -0,0 +1,18 @@
+﻿using HorizonFutureVest.BusinessLogic.DTOs;
+
+namespace HorizonFutureVest.BusinessLogic.Interfaces;
+
+public interface ISimulacionService
+{
+    Task<IEnumerable<MacroIndicadorSimulacionDto>> GetMacroindicadoresSimulacionAsync();
+    Task<MacroIndicadorSimulacionDto> GetSimulacionByIdAsync(int id);
+    Task<MacroIndicadorSimulacionDto> AgregarMacroindicadorAsync(int macroindicadorId, decimal peso);
+    Task<MacroIndicadorSimulacionDto> ActualizarPesoAsync(int id, decimal nuevoPeso);
+    Task<bool> EliminarMacroIndicadorAsync(int id);
+    Task<decimal> GetSumaPesosSimulacionAsync();
+    Task<bool> PuedeAgregarMasAsync();
+    Task<bool> ValidarPesoSimulacionAsync(decimal nuevoPeso, int? excludeId = null);
+    Task<IEnumerable<MacroIndicadorDto>> GetMacroIndicadoresDisponiblesAsync();
+    Task<bool> YaExisteEnSimulacionAsync(int macroindicadorId, int? excludeId = null);
+    Task LimpiarSimulacionAsync();
+}
