1d3516076c1a78c6bf133803e3762cdbf56c9534
AlnaDesarroller <natansanchez732@gmail.com>
2025-09-29 13:16:32 -0400

    Primer update de los controladores

---
+++ HorizonFutureVest.Web/Controllers/MacroIndicadorController.cs
@@ -0,0 +1,16 @@
+﻿using HorizonFutureVest.BusinessLogic.Interfaces;
+using HorizonFutureVest.BusinessLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HorizonFutureVest.Controllers;
+
+public class MacroIndicadorController : Controller
+{
+    private readonly MacroIndicadorService  _macroIndicadorService;
+    private readonly IMacroIndicadorService _indicadorService;
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ HorizonFutureVest.Web/Controllers/PaisController.cs
@@ -0,0 +1,25 @@
+﻿using HorizonFutureVest.BusinessLogic.Interfaces;
+using HorizonFutureVest.BusinessLogic.Services;
+using HorizonFutureVest.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HorizonFutureVest.Controllers;
+
+public class PaisController : Controller
+{
+    private readonly PaisService  _service;
+   
+
+    public PaisController(PaisService service)
+    {
+        _service = service;
+    }
+
+    // GET
+    public IActionResult Index()
+    {
+        var viewModel = new PaisViewModel();
+        return View(viewModel);
+    }
+    
+}
